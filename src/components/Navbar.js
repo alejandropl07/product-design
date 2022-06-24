07fa0b10d3dfc84de28bcdcd0556212b892978d6
manuBGUser <parabitcoin97@gmail.com>
2022-06-24 04:21:34 +0200

    Some navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,6 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import logo from "../assets/logo.png";
 import { Badge } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
@@ -13,6 +12,14 @@ import { auth } from "../firebase";
 import { emptyBasketAction } from "../actions/basketAction";
 import { setUserAction } from "../actions/userAction";
 import { useDispatch, useSelector } from "react-redux";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import CloseIcon from "@mui/icons-material/Close";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import PersonOutlineSharpIcon from "@mui/icons-material/PersonOutlineSharp";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 export default function Navbar() {
   const dispatch = useDispatch();
@@ -22,7 +29,7 @@ export default function Navbar() {
   const setUser = (id) => dispatch(setUserAction(id));
 
   const { basket } = useSelector((state) => state.basket);
-  const { user } = useSelector((state) => state.basket);
+  const { user } = useSelector((state) => state.user);
 
   const handleSignOut = () => {
     if (user) {
@@ -44,8 +51,10 @@ export default function Navbar() {
             <IconButton
               size="large"
               edge="start"
+              id="logo-button"
               color="inherit"
               aria-label="menu"
+              disableRipple="true"
               sx={{ mr: 2 }}
             >
               <img
@@ -53,11 +62,42 @@ export default function Navbar() {
                 alt="eCommerce"
                 style={{
                   marginRight: "1rem",
-                  height: "3rem",
+                  height: "4.5rem",
                 }}
               />
             </IconButton>
           </Link>
+          {/* <LocalPhoneIcon
+            fontSize="large"
+            sx={{ boxShadow: "none", color: "black" }}
+          />
+          <Typography
+            variant="h6"
+            color="textPrimary"
+            component="p"
+            sx={{ marginLeft: ".5rem", color: "black" }}
+          >
+            53581322
+          </Typography> */}
+          <SearchIcon
+            fontSize="large"
+            sx={{ boxShadow: "none", color: "black" }}
+          />
+          <TextField
+            placeholder="Que estas buscando..."
+            variant="standard"
+            size="5rem"
+            sx={{ marginLeft: "1rem", verticalAlign: "center" }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton sx={{ visibility: "hidden" }}> {/* mostrar cruz de limpiar si tiene algo escrito */}
+                    <CloseIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
           <Box sx={{ flexGrow: 1 }} />
           <Typography
             variant="h6"
@@ -65,23 +105,71 @@ export default function Navbar() {
             component="p"
             sx={{ marginRight: "1rem" }}
           >
-            Hello {user ? user.email : "Guest"}
+            {/* Hello {user ? user.email : "Guest"} */}
+            {user ? user.email : ""}
           </Typography>
-
-          <Link to="/sign-in">
+          {/* <Link to="/sign-in">
             <Button variant="outlined" onClick={handleSignOut}>
               <strong> {user ? "Sign Out" : "Sign In"} </strong>
             </Button>
+          </Link> */}
+          <Link
+            to={!user ? "/sign-in" : "/user-details" }
+          >
+            <IconButton
+              aria-label="show-cart-items"
+              color="inherit"
+              disableRipple="true"
+            >
+              <PersonOutlineSharpIcon
+                sx={{ fontSize: 45 }}
+                color="primary"
+                className="icon-hover"
+              />
+            </IconButton>
           </Link>
-
+          <Link to="/checkout-page">
+            <IconButton
+              aria-label="show-cart-items"
+              color="inherit"
+              disableRipple="true"
+            >
+              <Badge
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                badgeContent={basket.length}
+                color="info"
+              >
+                <FavoriteBorderIcon
+                  sx={{ fontSize: 40 }}
+                  color="primary"
+                  className="icon-hover"
+                />
+              </Badge>
+            </IconButton>
+          </Link>
           <Link to="/checkout-page">
             <IconButton
               aria-label="show-cart-items"
               color="inherit"
               sx={{ marginRight: "1rem" }}
+              disableRipple="true"
             >
-              <Badge badgeContent={basket.length} color="secondary">
-                <ShoppingCartIcon fontSize="large" color="primary" />
+              <Badge
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                badgeContent={basket.length}
+                color="info"
+              >
+                <ShoppingCartOutlinedIcon
+                  sx={{ fontSize: 40 }}
+                  color="primary"
+                  className="icon-hover"
+                />
               </Badge>
             </IconButton>
           </Link>
