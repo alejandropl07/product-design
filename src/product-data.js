77d532c9c97d8bdb132a2dd8c602eba0d861a16c
alejandropl07 <aprietoleon97@gmail.com>
2022-06-23 23:09:49 -0500

    Card Product modified

--- src/product-data.js
+++ src/product-data.js
@@ -26,6 +26,42 @@ const products  = [
         image: "https://ipadizate.com/hero/2022/01/iPhone-8-negro.jpg?width=480&aspect_ratio=19:10&format=nowebp",
         description: "Un Iphone"
     },
+    {
+        id: 4,
+        name: "Shoes",
+        productType: "Vans shoes",
+        price: 50,
+        rating: 4,
+        image: "https://i.pinimg.com/originals/63/81/8e/63818e317804c0695b615eb533b67195.jpg",
+        description: "Quieres comprar un estilo que nunca pasa de moda? Mira las zapatillas Vans clásicas slip on y las Old Skool, auténticos iconos."
+    },
+    {
+        id: 5,
+        name: "Shoes",
+        productType: "Vans shoes",
+        price: 50,
+        rating: 4,
+        image: "https://i.pinimg.com/originals/63/81/8e/63818e317804c0695b615eb533b67195.jpg",
+        description: "Quieres comprar un estilo que nunca pasa de moda? Mira las zapatillas Vans clásicas slip on y las Old Skool, auténticos iconos."
+    },
+    {
+        id: 6,
+        name: "Shoes",
+        productType: "Vans shoes",
+        price: 50,
+        rating: 4,
+        image: "https://i.pinimg.com/originals/63/81/8e/63818e317804c0695b615eb533b67195.jpg",
+        description: "Quieres comprar un estilo que nunca pasa de moda? Mira las zapatillas Vans clásicas slip on y las Old Skool, auténticos iconos."
+    },
+    {
+        id: 7,
+        name: "Shoes",
+        productType: "Vans shoes",
+        price: 50,
+        rating: 4,
+        image: "https://i.pinimg.com/originals/63/81/8e/63818e317804c0695b615eb533b67195.jpg",
+        description: "Quieres comprar un estilo que nunca pasa de moda? Mira las zapatillas Vans clásicas slip on y las Old Skool, auténticos iconos."
+    },
 ]
 
 export default products;
